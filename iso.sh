ceeb0fbca13b36659dcb8569d6da8e44b97d78f3
Trey Tomes <trey.tomes@gmail.com>
2021-09-03 15:01:17 -0500

    Skip the boot menu.

--- iso.sh
+++ iso.sh
@@ -8,8 +8,12 @@ mkdir -p isodir/boot/grub
 
 cp sysroot/boot/cocos.kernel isodir/boot/cocos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
+set timeout=0
+set default=0
+
 menuentry "cocos" {
 	multiboot /boot/cocos.kernel
+	boot
 }
 EOF
 grub-mkrescue -o cocos.iso isodir
